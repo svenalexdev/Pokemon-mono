2e8460183681125cb18cbe18374ab5d04e3f56a9
Sven Kaack <sven.kaack@gmx.de>
2025-06-18 00:34:19 +0200

    created Leaderboard model

--- Backend/models/Leaderboard.js
+++ Backend/models/Leaderboard.js
@@ -1,2 +1,25 @@
-import {Schema, model} from 'mongoose';
+import { Schema, model } from "mongoose";
 
+const leaderboardSchema = new Schema(
+    {
+        trainerName: {
+            type: String,
+            required: [true, "Trainer name is required"],
+        },
+        playerPokemon: {
+            type: String,
+            required: [true, "Player Pokemon is required"],
+        },
+        rivalPokemon: {
+            type: String,
+            required: [true, "Rival Pokemon is required"],
+        },
+        winningStreak: {
+            type: Number,
+            required: [true, "Winning streak is required"],
+        },
+    },
+    { timestamps: true }
+);
+
+export default model("Leaderboard", leaderboardSchema);
