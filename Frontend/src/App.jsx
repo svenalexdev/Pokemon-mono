ff52735c1c98a5447c2c4b55ff55162269398850
Stradow <karydis_kostas@icloud.com>
2025-06-18 18:21:08 +0300

    homepage mini update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import HomePage from './pages/HomePage';
 
 function App() {
- 
-
   return (
     <>
-       
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
