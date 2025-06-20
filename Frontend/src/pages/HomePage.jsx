75388981229312119a65d60b055a3393e73d3d39
Stradow <karydis_kostas@icloud.com>
2025-06-20 17:26:46 +0300

    login page update

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,11 +1,27 @@
+import { useNavigate } from 'react-router';
+
 function Homepage() {
+  const navigate = useNavigate();
+
+  const goToPokedex = () => {
+    navigate('pokedex');
+  };
+
+  const goToLoginPage = () => {
+    navigate('login');
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-indigo-100 to-blue-200 flex items-center justify-center p-6 text-black relative">
       <div className="absolute right-6 top-1/3 flex flex-col space-y-4">
-        <button className="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600">Battle</button>
+        <button onClick={goToLoginPage} className="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600">
+          Login
+        </button>
         <button className="bg-yellow-500 text-white py-2 px-4 rounded-lg hover:bg-yellow-600">Leaderboard</button>
-        <button className="bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-900">Login</button>
-        <button className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600">Register</button>
+        <button onClick={goToPokedex} className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600">
+          Pokedex
+        </button>
+        <button className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600">How to Play</button>
       </div>
 
       <div className="max-w-5xl w-full flex flex-col items-center space-y-10">
--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,70 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+
 function LoginPage() {
-  return;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async e => {
+    try {
+      e.preventDefault();
+      if (!email || !password) throw new Error('Fill all fields');
+      setLoading(true);
+      const { message } = await testSign({ email, password });
+      toast.success(message || 'Welcome back trainer');
+      navigate('/');
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const testSign = ({ email }) => {
+    return new Promise(res => setTimeout(() => res({ message: `Trainer ${email} logged in.` }), 1000));
+  };
+
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-indigo-100 to-blue-200 flex items-center justify-center text-black p-4">
+      <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md relative">
+        <h1 className="text-2xl font-bold mb-4">Login to Your Account</h1>
+        <button
+          onClick={() => navigate('/')}
+          className="absolute top-6 right-4 bg-indigo-600 text-white px-3 py-1 text-sm rounded hover:bg-indigo-700"
+        >
+          Back Home
+        </button>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Email"
+            className="p-2 border border-gray-300 rounded text-black"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-2 border border-gray-300 rounded text-black"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            disabled={loading}
+            className="bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default LoginPage;
