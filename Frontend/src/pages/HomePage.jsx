2a7f37a008c304e44ac4ec393294a7ca7d7415dd
marcochippy <hello@marcoczirpek.com>
2025-06-22 22:39:11 +0200

    semi final push

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -22,25 +22,41 @@ function Homepage() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-indigo-100 to-blue-200 flex items-center justify-center p-6 text-black relative">
-      <div className="absolute right-6 top-1/3 flex flex-col space-y-4">
-        <button onClick={goToLoginPage} className="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600">
-          Login
-        </button>
-        <button
-          onClick={goToLeaderboardPage}
-          className="bg-yellow-500 text-white py-2 px-4 rounded-lg hover:bg-yellow-600"
-        >
-          Leaderboard
-        </button>
-        <button onClick={goToPokedex} className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600">
-          Pokedex
-        </button>
-        <button onClick={goToHowToPlay} className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600">
-          How to Play
-        </button>
+    <div className="min-h-screen bg-gradient-to-b from-indigo-100 to-blue-200 p-6 text-black relative">
+      <div className="w-[1700px] mx-auto">
+        <img src="/PokeBrawlLogo.png" alt="" className="mx-auto w-70 drop-shadow-xl mb-5" />
+
+        <div className="flex ">
+          <div className="mx-auto space-x-8">
+            <button
+              onClick={goToLoginPage}
+              className="bg-gray-500 text-white py-2 px-4 rounded-lg cursor-pointer transition-all duration-400 hover:bg-green-500"
+            >
+              Login
+            </button>
+            <button
+              onClick={goToLeaderboardPage}
+              className="bg-gray-500 text-white py-2 px-4 rounded-lg cursor-pointer transition-all duration-400 hover:bg-yellow-500"
+            >
+              Leaderboard
+            </button>
+            <button
+              onClick={goToPokedex}
+              className="bg-gray-500 text-white py-2 px-4 rounded-lg cursor-pointer transition-all duration-400 hover:bg-gray-600"
+            >
+              Pokedex
+            </button>
+            <button
+              onClick={goToHowToPlay}
+              className="bg-gray-500 text-white py-2 px-4 rounded-lg cursor-pointer transition-all duration-400 hover:bg-red-500"
+            >
+              How to Play
+            </button>
+          </div>
+        </div>
+
+        <Game />
       </div>
-      <Game />
     </div>
   );
 }
